c7fc3bf100541bfe31bc8aad394c1ed502bcc8c1
ccczzc <ccczzc@github.com>
2024-12-16 22:04:15 -0500

    Refactor WiFi UDP FCFS source: update class structure, enhance sensor data handling, and improve command-line argument parsing

--- wifi_udp_fcfs_destination.py
+++ wifi_udp_fcfs_destination.py
@@ -1,42 +1,43 @@
 import argparse
+from collections import defaultdict
 from io import TextIOWrapper
 import os
 import select
 import socket
 import time
 from typing import List, Tuple
-from sensor import SensorData
+from sensor import DataType, SensorData
 
 class SourceState:
-    def __init__(self, output_fd: TextIOWrapper):
+    def __init__(self, output_fd: TextIOWrapper = None):
         self.weight: float = 0
         self.last_systime_received: float = time.time()
-        self.approximate_systime_HOL: float = 0
-        self.fragments: list[str] = []
-        self.time_poll_packets: list[float] = []
-        self.time_received_packets: list[float] = []
-        self.time_period: str = 0.5
         self.output_fd = output_fd
+        self.last_recorded_age = 0.0
+        self.total_weighted_ages: float = 0.0
 
-class WiFreshDestination:
+class WiFiUDPFcfsDestination:
     def __init__(
         self, 
-        sources_addresses: List[Tuple[str, int]], 
+        sources_addresses: List[Tuple[str, int, DataType]], 
         listen_port=9999, 
-        age_record_dir='./ages_multisource_wifi',
+        age_record_dir='./ages_wifi_udp_fcfs',
         age_record_interval=1e-4
     ):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', listen_port))
-        self.sources_state: dict[Tuple[str, int], SourceState] = {}
+        self.sources_state: dict[Tuple[str, int, DataType], SourceState] = defaultdict(SourceState)
+        self.age_record_dir = age_record_dir
         os.makedirs(age_record_dir, exist_ok=True)
         for source_address in sources_addresses:
-            source_file_path = os.path.join(age_record_dir, f"{source_address[0]}_{source_address[1]}.txt")
-            with open(source_file_path, 'w'):
-                pass
-            self.sources_state[source_address] = SourceState(output_fd=open(source_file_path, 'a'))
+            # source_file_path = os.path.join(age_record_dir, f"{source_address[0]}_{source_address[1]}.txt")
+            # with open(source_file_path, 'w'):
+            #     pass
+            self.sources_state[source_address] = SourceState()
         self.age_record_interval = age_record_interval  # Age record interval
         self.last_age_record_time = time.time() - self.age_record_interval
+        self.start_time = time.time()
+        self.running_period = 600.0  # 10 minutes in seconds
 
     def start(self):
         print("WiFi UDP FCFS destination started")
@@ -45,57 +46,81 @@ class WiFreshDestination:
             self.receive_response()
             if time.time() - self.last_age_record_time >= self.age_record_interval:
                 self.record_age()
-                
+            if time.time() - self.start_time >= self.running_period:
+                self.save_ages()
+                print("WiFi UDP FCFS destination stopped")
+                break
+            
+    def save_ages(self):
+        record_file_path = os.path.join(self.age_record_dir, f"ages_{len(self.sources_state)}sources.txt")
+        with open(record_file_path, 'w') as record_file:
+            mean_ages = []
+            for source_address, source in self.sources_state.items():
+                mean_age = source.total_weighted_ages / self.running_period
+                record_file.write(f"{source_address[0]}_{source_address[1]}_{source_address[2]}: {mean_age}\n")
+                mean_ages.append(mean_age)
+            record_file.write(f"Mean AOI of all data sources: {sum(mean_ages) / len(mean_ages)}\n")
+                    
     def record_age(self):
         for source in self.sources_state.values():
             current_time = time.time()
-            source.output_fd.write(f"{current_time: .8f}, {current_time - source.last_systime_received: .7f}\n")
+            age = current_time - source.last_systime_received
+            age_area = (age + source.last_recorded_age) * (current_time - self.last_age_record_time) / 2
+            source.total_weighted_ages += age_area
+            source.last_recorded_age = age
         self.last_age_record_time = time.time()
 
     def receive_response(self):
         readable, _, _ = select.select([self.sock], [], [], 0)
         if readable:
-            data, addr = self.sock.recvfrom(4096)
-            if addr not in self.sources_state:
-                print(f"Received data from unknown source {addr}: {data.decode()}")
-                exit(1)
-            data_str = data.decode()
-            # print(f"Received data from {addr}: {data_str}")
-            if data_str.startswith('TIME_REQUEST'):
-                parts = data_str.split(':')
-                if len(parts) == 2:
-                    source_time = float(parts[1])
-                    # Handle time synchronization request
-                    current_time = time.time()
-                    response = f"TIME_RESPONSE:{current_time:010.15f}:{source_time:010.15f}"
-                    self.sock.sendto(response.encode(), addr)
-                    print(f"Sent TIME_RESPONSE to {addr}: {current_time}")
+            data_bytes, addr = self.sock.recvfrom(4096*4096)
+            print(f"Received data from {addr}, size {len(data_bytes)}")
+            data_structed = SensorData.from_bytes(data_bytes)
+            # print(f"Received data from {addr}: {data_structed}")
+            if data_structed.data_type == DataType.TIME_REQUEST:
+                source_time = data_structed.timestamp
+                # Handle time synchronization request
+                current_time = time.time()
+                response = f"TIME_RESPONSE:{current_time:010.15f}:{source_time:010.15f}"
+                self.sock.sendto(response.encode(), addr)
+                print(f"Sent TIME_RESPONSE to {addr}: {current_time}")
             else:
-                self.process_fragment(data_str, addr)
+                # Assuming the type can be inferred from the data_structed
+                source_type = data_structed.data_type
+                addr_with_type = (addr[0], addr[1], source_type)
+                self.process_fragment(data_structed, addr_with_type)
+            
 
-    def process_fragment(self, fragment: str, source_addr):
-        fresh_fragment = SensorData.from_str(fragment)
+    def process_fragment(self, fresh_data: SensorData, source_addr):
+        if fresh_data is None:
+            return
         source = self.sources_state[source_addr]
-        # Adjust the timestamp
-        fragment_timestamp = fresh_fragment.timestamp
+        source = self.sources_state[source_addr]
         # Update last_systime_received
-        if source.last_systime_received < fragment_timestamp < time.time():
-            source.last_systime_received = fragment_timestamp
+        fresh_data.timestamp = max(fresh_data.timestamp, time.time())
+        if source.last_systime_received < fresh_data.timestamp:
+            source.last_systime_received = fresh_data.timestamp
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Start WiFreshDestination')
-    parser.add_argument('--sources', nargs='+', help='List of source addresses in the format ip:port')
+    parser.add_argument('--sources', nargs='+', help='List of source addresses in the format ip:port:type')
+    parser.add_argument('--listen_port', type=int, default=9999, help='Port to listen on')
+    parser.add_argument('--age_record_dir', default='./ages_wifresh_app', help='Directory to store age records')
     args = parser.parse_args()
 
     sources_addresses = []
     if args.sources:
         for src in args.sources:
-            ip, port = src.split(':')
-            sources_addresses.append((ip, int(port)))
+            ip, port, type = src.split(':')
+            sources_addresses.append((ip, int(port), DataType[type.upper()]))
     else:
         print("No sources specified")
-        print("Usage: python destination.py --sources <ip:port> <ip:port> ...")
+        print("Usage: python destination.py --sources <ip:port:type> <ip:port:type> ...")
         exit(1)
 
-    destination = WiFreshDestination(sources_addresses=sources_addresses)
+    destination = WiFiUDPFcfsDestination(
+        sources_addresses=sources_addresses,
+        listen_port=args.listen_port,
+        age_record_dir=args.age_record_dir
+    )
     destination.start()
--- wifi_udp_fcfs_source.py
+++ wifi_udp_fcfs_source.py
@@ -1,53 +1,57 @@
+import argparse
+from collections import defaultdict
 import select
 import socket
-import threading
 import time
-from sensor import Sensor, SensorData
-import sys
+from typing import List
+from sensor import Sensor, SensorData, DataType
 
-class WiFreshSource:
+class WiFiUDPFcfsSource:
     def __init__(
         self, 
         listen_port, 
         destination_address,
-        generation_rate=100,
+        sensor_list: List[Sensor],
         sync_interval=5,
         sync_rounds=5,
         clock_offset_alpha=0.02
         ):
         self.listen_port = listen_port
         self.destination_address = destination_address
-        self.fcfs_queue: list[SensorData] = []  # FCFS fragment queue
-        self.sensor = Sensor(listen_port)  # Sensor instance
-        self.packet_header_size = 20  # Packet header size
-        self.max_packet_size = self.get_max_packet_size() - self.packet_header_size  # Get maximum packet size
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('0.0.0.0', self.listen_port))
-        self.generation_rate = generation_rate  # Generation rate
+        self.sensor_list = sensor_list
         self.clock_offset = 0.0  # Clock offset
         self.sync_interval = sync_interval  # Clock synchronization interval (seconds)
         self.last_sync_time = time.time()
         self.sync_rounds = sync_rounds  # Number of messages per synchronization
         self.clock_offset_alpha = clock_offset_alpha  # Smoothing factor for clock offset adjustment (0 < alpha <= 1)
 
-    def get_max_packet_size(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        max_packet_size = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-        sock.close()
-        return max_packet_size
-
     def start(self):
         print(f"WiFi UDP FCFS source started on port {self.listen_port}")
         self.sock.setblocking(False)
-        last_generation_time = time.time()
-        generation_interval = 1.0 / self.generation_rate
         while True:
+            # Handle received messages
+            self.receive_response()
             # Check if clock synchronization is needed
             if time.time() - self.last_sync_time >= self.sync_interval:
                 self.clock_synchronization()
-                self.last_sync_time = time.time()
 
-            # Handle received messages
+            for sensor in self.sensor_list:
+                # Try generate sensor data
+                sensor.generate_data()
+                if sensor.complete_data_queue:
+                    # Adjust timestamp using clock offset
+                    oldest_data = sensor.complete_data_queue[0]
+                    oldest_data.timestamp += self.clock_offset
+                    try:
+                        self.send_packet(oldest_data)
+                        sensor.complete_data_queue.pop(0)
+                    except BlockingIOError:
+                        print("source run send_packet BlockingIOError")
+                        oldest_data.timestamp -= self.clock_offset
+
+    def receive_response(self):
             readable, _, _ = select.select([self.sock], [], [], 0)
             if readable:
                 data, addr = self.sock.recvfrom(1024)
@@ -65,43 +69,44 @@ class WiFreshSource:
                         print(f"Updated clock offset: {self.clock_offset} seconds")
                 else:
                     print(f"Received unknown message from {addr}: {data_str}")
-                    
-            now = time.time()
-            if now - last_generation_time >= generation_interval:
-                # Generate sensor data
-                info_update = self.sensor.generate_data()
-                # Adjust timestamp using clock offset
-                info_update.timestamp += self.clock_offset
-                self.fcfs_queue.append(info_update)
-                try:
-                    self.send_packet(self.fcfs_queue[0])
-                    self.fcfs_queue.pop(0)
-                except BlockingIOError:
-                    print("source run send_packet BlockingIOError")
-                last_generation_time = now
 
     def send_packet(self, packet: SensorData):
-        bytes_sent = self.sock.sendto(packet.to_str().encode(), self.destination_address)
+        bytes_sent = self.sock.sendto(packet.to_bytes(), self.destination_address)
         # print(f"Sent {bytes_sent} bytes to {self.destination_address}")
 
     def clock_synchronization(self):
         for _ in range(self.sync_rounds):
             # Send TIME_REQUEST to the destination
-            current_time = time.time()
-            request = f"TIME_REQUEST:{current_time:010.15f}"
             try:
-                self.sock.sendto(request.encode(), self.destination_address)
+                current_time = time.time()
+                request = SensorData(is_fragmented=0, data_type=DataType.TIME_REQUEST, timestamp=current_time, data=b'')
+                self.sock.sendto(request.to_bytes(), self.destination_address)
             except BlockingIOError:
                 print("source clock_synchronization sendto BlockingIOError")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python source.py <listen_port> <destination_host> <destination_port>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Start Source')
+    parser.add_argument('--listen_port', type=int, required=True, help='Port to listen on')
+    parser.add_argument('--destination', required=True, help='Destination address in the format ip:port')
+    parser.add_argument('--sensors', nargs='+', required=True, help='Sensor configurations in the format type:size:frequency')
+    args = parser.parse_args()
 
-    listen_port = int(sys.argv[1])
-    destination_host = sys.argv[2]
-    destination_port = int(sys.argv[3])
-    destination_address = (destination_host, destination_port)
-    source = WiFreshSource(listen_port=listen_port, destination_address=destination_address)
+    dest_ip, dest_port = args.destination.split(':')
+    destination_address = (dest_ip, int(dest_port))
+
+    # Parse sensor configurations
+    sensor_list = []
+    for sensor_arg in args.sensors:
+        sensor_type_str, size_str, frequency_str = sensor_arg.split(':')
+        sensor_type = DataType[sensor_type_str.upper()]
+        size = int(size_str)
+        frequency = float(frequency_str)
+        sensor_list.append(Sensor(sensor_type, size, frequency))
+        print(f"Added sensor: {sensor_type} - packet size: {size} - frequency: {frequency}")
+
+    source = WiFiUDPFcfsSource(
+        listen_port=args.listen_port,
+        destination_address=destination_address,
+        sensor_list=sensor_list
+    )
     source.start()
